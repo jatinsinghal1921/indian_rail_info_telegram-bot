680883f623000c0fa6a9c77191110ec6c71077f5
jatinsinghal1921 <jtn1921@gmail.com>
2019-01-01 21:34:40 +0530

    added functioanlities in get_rail_info.py and added caller functions in bot_framework.py

--- bot_framework.py
+++ bot_framework.py
@@ -1,14 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from get_rail_info import get_availability_info, pnr_status
-
+from get_rail_info import RailInfoDayCount, RailInfoCreditCount
 
 # Global Variables
 TelegramAPI_token = "Your Telegram Token"
-
-railways_api_url = "https://api.railwayapi.com/v2/"
-pnr_url = "https://api.railwayapi.com/v2/live/train/<train number>/date/<dd-mm-yyyy>/apikey/<apikey>/"
-Seat_availability_url = "https://api.railwayapi.com/v2/check-seat/train/<train number>/source/<stn code>/dest/<dest code>/date/<dd-mm-yyyy>/pref/<class code>/quota/<quota code>/apikey/<apikey>/"
-
 
 # Creating Updater and Dispatcher objects
 updater = Updater(TelegramAPI_token)
@@ -22,40 +16,133 @@ def hello_handler(bot, update):
 
 
 # Defining "PNR Status" Handler Function 
-def pnr_status_handler(bot, update,args):
-	print("In pnr function")
-	if (not args):
-		bot.send_message(chat_id=update.message.chat_id, text="Please enter the PNR number")
-		return -1
-	elif(len(args) >= 2):
-		bot.send_message(chat_id=update.message.chat_id, text="Please enter the single valid PNR number")
-		return -1
+def pnr_status_handler(bot, update, args):
+    print("In pnr function")
+    if not args:
+        bot.send_message(chat_id=update.message.chat_id, text="Please enter the PNR number")
+        return -1
+    elif len(args) >= 2:
+        bot.send_message(chat_id=update.message.chat_id, text="Please enter the single valid PNR number")
+        return -1
 
-	pnr = str(args[0])
-	print(pnr)
-	output_text = pnr_status(pnr)
-	bot.send_message(chat_id=update.message.chat_id, text=output_text)
+    pnr = str(args[0])
+    print(pnr)
+    obj = RailInfoCreditCount()
+    output_text = obj.pnr_status(pnr)
+    bot.send_message(chat_id=update.message.chat_id, text=output_text)
+
+
+# Defining "Seat Availability" Handler Function
+def seat_availability_handler(bot, update, args):
+    print("In Seat Availability function")
+    if len(args) < 6:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter all the entries required for seat avalability")
+        return -1
+    elif len(args) > 6:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter valid entries required for seat avalability")
+        return -1
+    train_no = str(args[0])
+    src_stn = str(args[1])
+    des_stn = str(args[2])
+    date = str(args[3])
+    class_code = str(args[4])
+    quota = str(args[5])
+    print(train_no, src_stn, des_stn, date, class_code, quota)
+
+    obj = RailInfoCreditCount()
+    availability_info = obj.seat_availability_info(train_no, src_stn, des_stn, date, class_code, quota)
+    bot.send_message(chat_id=update.message.chat_id, text=availability_info)
 
 
 # Defining "Train Availability" Handler Function
 def train_availability_handler(bot, update, args):
-	train_no = str(args[0])
-	src_stn = str(args[1])
-	des_stn = str(args[2])
-	date = str(args[3])
-	class_code = str(args[4])
-	quota = str(args[5])
+    print("In Train Availability function")
+    if len(args) < 3:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter all the entries required for train avalability")
+        return -1
+    elif len(args) > 3:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter valid entries required for train avalability")
+        return -1
+    src_stn = str(args[0])
+    des_stn = str(args[1])
+    date = str(args[2])
+    print(src_stn, des_stn, date)
 
-	availability_info = get_availability_info(train_no,src_stn,des_stn,date,class_code,quota)
-	bot.send_message(chat_id=update.message.chat_id, text=availability_info)
+    obj = RailInfoCreditCount()
+    availability_info = obj.train_availability_info(src_stn, des_stn, date)
+    for each_train in availability_info:
+        bot.send_message(chat_id=update.message.chat_id, text=each_train)
+    del obj
+
+
+def train_fare_handler(bot, update, args):
+    print("In Train Fare function")
+    if len(args) < 4:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter all the entries required to get the Train Fare")
+        return -1
+    elif len(args) > 4:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter valid entries required to get the Train Fare")
+        return -1
+    train_no = str(args[0])
+    src_stn = str(args[1])
+    des_stn = str(args[2])
+    quota = str(args[3])
+    print(train_no, src_stn, des_stn, quota)
+
+    obj = RailInfoDayCount()
+    fare_info = obj.train_fare(train_no, src_stn, des_stn, quota)
+    bot.send_message(chat_id=update.message.chat_id, text=fare_info)
+
+
+def track_train_handler(bot, update, args):
+    print("In Train Tracking function")
+    if len(args) < 2:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter all the entries required to track the train")
+        return -1
+    elif len(args) > 2:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter valid entries required to track the train")
+        return -1
+    train_no = str(args[0])
+    date = str(args[1])
+    print(train_no, date)
+
+    obj = RailInfoCreditCount()
+    track_info = obj.track_train(train_no, date)
+    bot.send_message(chat_id=update.message.chat_id, text=track_info)
+
+
+def train_details_handler(bot, update, args):
+    print("In Train Details function")
+    if len(args) < 1:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Please enter either the train name or train number.")
+        return -1
+    elif len(args) > 1:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="There are too many entries. Please enter either Train no. or Train name.")
+        return -1
+    train_no = str(args[0])
+    print(train_no)
+
+    obj = RailInfoCreditCount()
+    train_info = obj.train_details(train_no)
+    bot.send_message(chat_id=update.message.chat_id, text=train_info)
 
 
 # Testing Message Handler Function
-def message_handler(bot,update):
-	net_args = update.message.text
-	net_args = net_args[::-1]
-	print("cool")
-	bot.send_message(chat_id=update.message.chat_id, text=net_args)
+def message_handler(bot, update):
+    net_args = update.message.text
+    net_args = net_args[::-1]
+    print("In message_handler function")
+    bot.send_message(chat_id=update.message.chat_id, text=net_args)
 
 
 # Handling Command Handler Errors
@@ -66,7 +153,11 @@ def unknown_command_handler(bot, update):
 # Calling Handler Functions
 dispatcher.add_handler(CommandHandler('hello', hello_handler))
 dispatcher.add_handler(CommandHandler('pnr', pnr_status_handler, pass_args=True))
+dispatcher.add_handler(CommandHandler('seatAvailability', seat_availability_handler, pass_args=True))
 dispatcher.add_handler(CommandHandler('trainAvailability', train_availability_handler, pass_args=True))
+dispatcher.add_handler(CommandHandler('train_fare', train_fare_handler, pass_args=True))
+dispatcher.add_handler(CommandHandler('track_train', track_train_handler, pass_args=True))
+dispatcher.add_handler(CommandHandler('train_details', train_details_handler, pass_args=True))
 dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
 dispatcher.add_handler(MessageHandler(Filters.command, unknown_command_handler))
 
--- get_rail_info.py
+++ get_rail_info.py
@@ -1,75 +1,209 @@
 import requests
 import json
 
-# Global Varaibles
-RailwaysAPI_token = "Your Railways API token"
-railways_api_url = "https://api.railwayapi.com/v2/"
+# Global variables
+railways_api_token_day_count = "Your API Token"
+railways_api_url_day_count = "http://indianrailapi.com/api/v2/"
+
+railways_api_url_credit_count = "https://api.railwayapi.com/v2/"
+railways_api_token_credit_count = "Your API Token"
+
+response_codes_file_obj = open("response_codes.json", "r")
+response_codes_dict = json.load(response_codes_file_obj)
 
 
-# Get PNR Status Info of all the passengers to a same ticket
-def pnr_status(pnr_no):
-    global railways_api_url
-    railways_api_url += "pnr-status/pnr/" + str(pnr_no) + "/apikey/" + RailwaysAPI_token + "/"
-    response = requests.get(railways_api_url)
-    print("Status Code : " + str(response.status_code))
-    
-    if response.status_code != 200:
-        print("Request Failed")
-        return -1
-    
-    json_output = response.json()
-    print(json_output)
-    passengers_pnrstatus_list = json_output["passengers"]
-    print("Length of pnrStatusList : " + str(len(passengers_pnrstatus_list)))
+class ErrorHandling:
+    def __init__(self):
+        print("In Error Handler Class")
 
-    output_str = "PNR Details : \n\n"
-    for entry in passengers_pnrstatus_list:
-        passenger_id = entry["no"]
-        current_status = entry["current_status"]
-        booking_status = entry["booking_status"]
-        output_str += "passenger : " + str(passenger_id) + "\n" + "Current Status : " + current_status + "\n" + "Booking Status : " + booking_status + "\n\n"
+    # Handling Errors
+    def error_handler(self, response):
+        print("In Error handler function")
+        print(response)
+        if response.status_code != 200:
+            output_str = "Status Code " + str(
+                response.status_code) + "\nThere is an error in serving your Request.\nPlease try again later. "
+            print(output_str)
+            return output_str
 
-    return output_str
+        json_output = response.json()
+        output_str = 1
+        try:
+            response_code = json_output['ResponseCode']
+            if str(response_code) != "200":
+                output_str = "Response Code " + str(response_code) + "\nThere is an Error in Response"
+        except KeyError as e:
+            print("There is a key error Exception, Key {} doesn't exists. Instead using 'response_code' as key".format(e))
+            response_code = json_output['response_code']
+            if str(response_code) != "200":
+                output_str = "Response Code " + str(response_code) + "\n" + response_codes_dict[str(response_code)]
+
+        print(output_str)
+        return output_str
 
 
-# Get Train Availability Info for specific train.
-def get_availability_info(train_no,src_stn_code,des_stn_code,date,class_code,quota_code):    
-    global railways_api_url
-    railways_api_url += "check-seat/train/" + str(train_no) + "/source/" + str(src_stn_code) + "/dest/" + str(des_stn_code) + "/date/" + str(date) + "/pref/" + str(class_code) + "/quota/" + str(quota_code) + "/apikey/" + RailwaysAPI_token + "/"
-    response = requests.get(railways_api_url)
-    
-    print("Status Code : " + str(response.status_code))
-    if response.status_code != 200:
-        print("Request Failed")
-        return -1
+class RailInfoDayCount(ErrorHandling):
+    # Constructor
+    def __init__(self):
+        print("New Object Created of Rail Info Day Count Class")
 
-    json_output = response.json()
-    availability_list = json_output['availability']
-    print("Length of Availability list : " + str(len(availability_list)))
+    # Train Fare
+    def train_fare(self, train_no, src_stn_code, des_stn_code, quota_code):
+        global railways_api_url_day_count, railways_api_token_day_count
+        request_url = railways_api_url_day_count + "TrainFare/apikey/" + railways_api_token_day_count + "/TrainNumber/" + train_no + "/From/" + src_stn_code + "/To/" + des_stn_code + "/Quota/" + quota_code
+        response = requests.get(request_url)
 
-    output_str = "Availability Details:\n"
-    for entry in availability_list:
-        date = entry['date']
-        status = entry['status']
-        output_str += date + " -----------> " + status + "\n"
+        status = self.error_handler(response)
+        if status != 1:
+            return status
 
-    return output_str
+        json_output = response.json()
+        train_fares = json_output["Fares"]
+        output_str = ""
+        for train_class in train_fares:
+            output_str += train_class["Name"] + " --------------> " + train_class["Fare"] + "\n"
+
+        print(output_str)
+        return output_str
+
+
+class RailInfoCreditCount(ErrorHandling):
+    def __init__(self):
+        print("New Object Created of Rail Info Credit Count")
+
+    # Get PNR Status Info of all the passengers to a same ticket
+    def pnr_status(self, pnr_no):
+        global railways_api_url_credit_count, railways_api_token_credit_count
+        request_url = railways_api_url_credit_count + "pnr-status/pnr/" + pnr_no + "/apikey/" + railways_api_token_credit_count + "/"
+        response = requests.get(request_url)
+
+        status = self.error_handler(response)
+        if status != 1:
+            return status
+
+        json_output = response.json()
+        passengers_pnr_status_list = json_output["passengers"]
+        passengers_count = len(passengers_pnr_status_list)
+        print("Passengers Count : " + str(passengers_count))
+
+        output_str = "PNR Details : \n\n"
+        for entry in passengers_pnr_status_list:
+            passenger_id = entry["no"]
+            current_status = entry["current_status"]
+            booking_status = entry["booking_status"]
+            output_str += "passenger : " + str(
+                passenger_id) + "\n" + "Current Status : " + current_status + "\n" + "Booking Status : " + booking_status + "\n\n"
+
+        print(output_str)
+        return output_str
+
+    # Get Seat Availability Info for specific train on specific date.
+    def seat_availability_info(self, train_no, src_stn_code, des_stn_code, date, class_code, quota_code):
+        global railways_api_url_credit_count, railways_api_token_credit_count
+        request_url = railways_api_url_credit_count + "check-seat/train/" + train_no + "/source/" + src_stn_code + "/dest/" + des_stn_code + "/date/" + date + "/pref/" + class_code + "/quota/" + quota_code + "/apikey/" + railways_api_token_credit_count + "/"
+        response = requests.get(request_url)
+
+        print("Response has been fetched and it is going to be validated")
+
+        status = self.error_handler(response)
+        if status != 1:
+            return status
+
+        json_output = response.json()
+        availability_list = json_output['availability']
+        print("Length of Availability list : " + str(len(availability_list)))
+
+        output_str = "Availability Details:\n"
+        for entry in availability_list:
+            date = entry['date']
+            status = entry['status']
+            output_str += date + " -----------> " + status + "\n"
+
+        print(output_str)
+        return output_str
+
+    # get the availability of trains
+    def train_availability_info(self, src_stn_code, des_stn_code, date):
+        global railways_api_url_credit_count, railways_api_token_credit_count
+        request_url = railways_api_url_credit_count + "between/source/" + src_stn_code + "/dest/" + des_stn_code + "/date/" + date + "/apikey/" + railways_api_token_credit_count + "/"
+        response = requests.get(request_url)
+
+        status = self.error_handler(response)
+        if status != 1:
+            return status
+
+        json_output = response.json()
+        trains_list = json_output["trains"]
+        print("NO. of trains : " + str(len(trains_list)))
+
+        trains_info = []
+        for train in trains_list:
+            l = ""
+            l += "Train : " + train["name"] + "(" + train["number"] + ")" + "\n"
+            l += "Source Stn : " + train["from_station"]["name"] + "\n"
+            l += "Destination Stn : " + train["to_station"]["name"] + "\n"
+            l += "Departure Time : " + train["src_departure_time"] + "\n"
+            l += "Arrival Time : " + train["dest_arrival_time"] + "\n"
+            l += "Total Time : " + train["travel_time"] + "\n"
+            trains_info.append(l)
+
+        print(trains_info)
+        return trains_info
+
+    # Track Train at run time
+    def track_train(self, train_no, date):
+        global railways_api_url_credit_count, railways_api_token_credit_count
+        request_url = railways_api_url_credit_count + "live/train/" + train_no + "/date/" + date + "/apikey/" + railways_api_token_credit_count + "/"
+        response = requests.get(request_url)
+
+        status = self.error_handler(response)
+        if status != 1:
+            return status
+
+        json_output = response.json()
+        output_str = json_output["position"]
+        print(output_str)
+        return output_str
+
+    # Track Train at run time
+    def train_details(self, train_name):
+        global railways_api_url_credit_count, railways_api_token_credit_count
+        request_url = railways_api_url_credit_count + "name-number/train/" + train_name + "/apikey/" + railways_api_token_credit_count + "/"
+        response = requests.get(request_url)
+
+        status = self.error_handler(response)
+        if status != 1:
+            return status
+
+        json_output = response.json()
+        output_str = json_output["train"]["name"] + " -----------> " + json_output["train"]["number"] + "\n" + "Running Details:" + "\n"
+        for day in json_output["train"]["days"]:
+            output_str += day["code"] + " ------------> " + day["runs"] + "\n"
+        print(output_str)
+        return output_str
 
 
 def main():
-    train_no = 12649
-    src_stn_code = "YPR"
-    des_stn_code = "KCG"
-    date = "24-12-2018"
-    class_code = "SL"
-    quota_code = "GN"
-    
-    availability_details = get_availability_info(train_no,src_stn_code,des_stn_code,date,class_code,quota_code)
-    print(availability_details)
+    train_no = "12723"
+    src_stn_code = "hyb"
+    des_stn_code = "ndls"
+    date = "01-01-2019"
+    class_code = "3a"
+    quota_code = "gn"
+    train_name = "Telangana Express"
+    pnr = "1234567890"
 
-    # pnr_no = "4561574060"
-    # pnr_details = pnr_status(pnr_no)
-    # print(pnr_details)
+    class1_obj1 = RailInfoDayCount()
+    # class1_obj1.track_train(train_no, "20190101")
+    # class1_obj1.train_fare(train_no, src_stn_code, des_stn_code, quota_code)
+
+    class2_obj1 = RailInfoCreditCount()
+    # class2_obj1.seat_availability_info(train_no, src_stn_code, des_stn_code, date, class_code, quota_code)
+    class2_obj1.train_availability_info(src_stn_code,des_stn_code,date)
+    # class2_obj1.pnr_status(pnr)
+    # class2_obj1.track_train(train_no,date)
+    # class2_obj1.train_details("AP Sampark Kranti")
+
 
 if __name__ == "__main__":
     main()
